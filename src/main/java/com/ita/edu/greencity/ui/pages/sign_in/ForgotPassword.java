cc872006fc7021c42857c0e68abf8f5dcf9f4ae5
Roman Kaniuka <kanuka878@gmail.com>
2022-01-20 22:26:13 +0200

    Initialized webdriver in 'ForgotPassword.java'

--- src/main/java/com/ita/edu/greencity/ui/pages/sign_in/ForgotPassword.java
+++ src/main/java/com/ita/edu/greencity/ui/pages/sign_in/ForgotPassword.java
@@ -1,10 +1,19 @@
 package com.ita.edu.greencity.ui.pages.sign_in;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
 public class ForgotPassword {
+    private WebDriver driver;
+
+    public ForgotPassword(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
     @FindBy(how = How.CSS, using = "a.close-modal-window")
     private WebElement close;
 
