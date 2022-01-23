82a88878558ef9bf5f11b1ebb8ab199fafbfd1f0
Tetiana Ustymenko <ustymenkotania@gmail.com>
2022-01-24 01:09:49 +0200

    Added new pages and test for confirmation stage

---
+++ src/main/java/com/ita/edu/greencity/ui/pages/orders/payment/Operation3dSecurePopUp.java
@@ -0,0 +1,29 @@
+package com.ita.edu.greencity.ui.pages.orders.payment;
+
+import com.ita.edu.greencity.ui.pages.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class Operation3dSecurePopUp extends BasePage {
+
+    @FindBy(how = How.XPATH, using = "//button")
+    private WebElement continueButton;
+    @FindBy(how = How.XPATH, using = "//div[@class = 'page-section page-section-header']/h1")
+    private WebElement pageTitle;
+
+    public Operation3dSecurePopUp(WebDriver driver) {
+        super(driver);
+    }
+
+    public SuccessfulOrderPage clickOnContinueButton() {
+        continueButton.click();
+        return new SuccessfulOrderPage(driver);
+    }
+
+    public String getTextFromPageTitle() {
+        return pageTitle.getText();
+    }
+
+}
--- src/test/java/com/ita/edu/greencity/tests/ui/FirstTest.java
+++ src/test/java/com/ita/edu/greencity/tests/ui/FirstTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class FirstTest extends TestRun {
     @Test
     public void firstTest() {
-        String expected = "https://ita-social-projects.github.io/GreenCityClient/#/";
+        String expected = "https://ita-social-projects.github.io/GreenCityClient/#/ubs";
         String actual = driver.getCurrentUrl();
         Assert.assertEquals(actual, expected);
     }
