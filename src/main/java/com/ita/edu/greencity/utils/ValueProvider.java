52797158bf1c04c6b80c4b49cbf998452525aa60
annachopyk <95702407+annachopyk@users.noreply.github.com>
2022-01-30 15:59:13 +0200

    add setPassword

--- src/main/java/com/ita/edu/greencity/utils/ValueProvider.java
+++ src/main/java/com/ita/edu/greencity/utils/ValueProvider.java
@@ -66,4 +66,13 @@ public class ValueProvider {
     public void setPassword(String newPassword) {
         properties.setProperty("password", newPassword);
     }
+    
+    public void setPassword(String newPassword) throws IOException {
+        FileReader reader =  new FileReader("src/main/resources/properties.properties");
+        properties.load(reader);
+        properties.getProperty("password");
+        properties.setProperty("password",newPassword);
+        OutputStream os = new FileOutputStream("src/main/resources/properties.properties");
+        properties.store(os,"new password for rollback" );
+    }
 }
