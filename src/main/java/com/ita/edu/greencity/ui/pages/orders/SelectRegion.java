0c0fd448f3c2094b2e0ca0ff729938310e05d209
Marta Khoma <tyxa.khoma16@gmail.com>
2022-02-06 12:53:09 +0300

    Added @Step annotation for SelectRegion page

--- src/main/java/com/ita/edu/greencity/ui/pages/orders/SelectRegion.java
+++ src/main/java/com/ita/edu/greencity/ui/pages/orders/SelectRegion.java
@@ -1,7 +1,7 @@
 package com.ita.edu.greencity.ui.pages.orders;
 
 import com.ita.edu.greencity.ui.pages.BasePage;
-import com.ita.edu.greencity.ui.pages.ubs_homepage.UbsHomePage;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -30,44 +30,50 @@ public class SelectRegion extends BasePage {
         super(driver);
     }
 
+    @Step("get title text from the  pop-up after logging in")
     public String getTitleText() {
         this.sleep(7000);
         return title.getText();
     }
-    public WebElement getCloseButton() {
-        return closeButton;
-    }
 
+    @Step("get bottom text from the  pop-up after logging in")
     public String getBottomText() {
         return bottomText.getText();
     }
 
+    @Step("get Continue button text from the  pop-up after logging in")
     public String getContinueButtonText() {
         return continueButton.getText();
     }
 
+    @Step("get Back button text from the  pop-up after logging in")
     public String getBackButtonText() {
         return backButton.getText();
     }
 
+    @Step("click on Close button at pop-up after logging in")
     public void clickOnCloseButton() {
         closeButton.click();
     }
 
+    @Step("click on Back button at pop-up after logging in")
     public void clickOnBackButton() {
         backButton.click();
     }
 
+    @Step("click on Continue button at pop-up after logging in")
     public OrderDetailsPage clickOnContinueButton() {
         waitUntilElementToBeClickable(By.xpath("//button[@class='btn primaryButton primary-global-button']"), 10);
         continueButton.click();
         return new OrderDetailsPage(driver);
     }
 
+    @Step("click on Select Region dropdown at pop-up after logging in")
     public void clickOnRegionDropdown() {
         regionDropdown.click();
     }
 
+    @Step("choose region by index {index}")
     public SelectRegion chooseRegionByIndex(int index) {
         sleep(5000);
         clickOnRegionDropdown();
@@ -75,6 +81,7 @@ public class SelectRegion extends BasePage {
         return this;
     }
 
+    @Step("choose region by String value {value}")
     public SelectRegion chooseRegionByValue(String value) {
         sleep(7000);
         clickOnRegionDropdown();
