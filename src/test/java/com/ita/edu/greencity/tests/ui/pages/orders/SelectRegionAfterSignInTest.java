706052602a58d6892629caed8d28131145caa277
Marta Khoma <tyxa.khoma16@gmail.com>
2022-02-06 13:05:49 +0300

    Added Test

--- src/test/java/com/ita/edu/greencity/tests/ui/pages/orders/SelectRegionAfterSignInTest.java
+++ src/test/java/com/ita/edu/greencity/tests/ui/pages/orders/SelectRegionAfterSignInTest.java
@@ -2,12 +2,14 @@ package com.ita.edu.greencity.tests.ui.pages.orders;
 
 import com.ita.edu.greencity.tests.ui.pages.testrunners.TestRun;
 import com.ita.edu.greencity.ui.pages.header.HeaderSignedOutComponent;
+import jdk.jfr.Description;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SelectRegionAfterSignInTest extends TestRun {
     public final static String region = "Kyiv";
 
+    @Description("Verify equality of regions from pop-up and OrderDetailsPage")
     @Test
     public void changeRegionAfterSignIn() {
         HeaderSignedOutComponent header = new HeaderSignedOutComponent(driver);
@@ -19,4 +21,18 @@ public class SelectRegionAfterSignInTest extends TestRun {
                 .clickOnContinueButton().getLocationFromTitle().substring(11).trim();
         Assert.assertEquals(actual, region);
     }
+
+    @Test
+    public void checkRedirectionAfterContinueButton() {
+        String expected = "https://ita-social-projects.github.io/GreenCityClient/#/ubs/order";
+        HeaderSignedOutComponent header = new HeaderSignedOutComponent(driver);
+        header.clickSignIn()
+                .inputEmail(provider.getEmail())
+                .inputPassword(provider.getPassword())
+                .clickSignIn()
+                .clickOnContinueButton();
+        String actual = driver.getCurrentUrl();
+        Assert.assertEquals(actual, expected);
+    }
+
 }
