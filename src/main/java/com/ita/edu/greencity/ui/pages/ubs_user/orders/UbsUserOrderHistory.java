535dcca9b83858f1e896e15f385d9ee2677c57cd
Ivan Hrabarskiy <ivan@example.com>
2022-02-07 01:39:35 +0200

    Created page Objects for UbsUserPage and created tests for UbsUser

--- src/main/java/com/ita/edu/greencity/ui/pages/ubs_user/orders/UbsUserOrderHistory.java
+++ src/main/java/com/ita/edu/greencity/ui/pages/ubs_user/orders/UbsUserOrderHistory.java
@@ -3,6 +3,7 @@ package com.ita.edu.greencity.ui.pages.ubs_user.orders;
 import com.ita.edu.greencity.ui.pages.BasePage;
 import com.ita.edu.greencity.ui.pages.orders.OrderDetailsPage;
 import com.ita.edu.greencity.ui.pages.ubs_user.UbsUser;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -20,16 +21,32 @@ public class UbsUserOrderHistory extends BasePage {
     @FindBy(how = How.XPATH, using = ".//*[contains(@tabindex, '0')]/*[contains(@class, 'mat-tab-label-content')]")
     private WebElement currentOrdersTabButton;
 
+    @FindBy(how = How.XPATH, using = ".//*[contains(@tabindex, '-1')]/*[contains(@class, 'mat-tab-label-content')]")
+    private WebElement orderHistoryTabButton;
+
+    @Step("click on new order button")
     public OrderDetailsPage clickOnNewOrderButton() {
         newOrderButton.click();
         return new OrderDetailsPage(driver);
     }
 
+    @Step("click on current order button")
     public UbsUserOrders clickOnCurrentOrdersButton() {
         currentOrdersTabButton.click();
         return new UbsUserOrders(driver);
     }
 
+    @Step("get element of current orders button")
+    public WebElement getCurrentOrdersButton() {
+        return currentOrdersTabButton;
+    }
+
+    @Step("get element of order history button")
+    public WebElement getOrderHistoryTabButton() {
+        return orderHistoryTabButton;
+    }
+
+    @Step("get orders page")
     public UbsUser getUbsUserPage() {
         return new UbsUser(driver);
     }
